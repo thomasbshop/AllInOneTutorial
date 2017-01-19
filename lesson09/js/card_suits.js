c5ec0c2becd13a7f69ed21fb6db5129f7093f20b
Thomas Juma <thomasbshop@gmail.com>
2017-01-19 16:25:36 +0300

    First Commit

---
+++ lesson09/js/card_suits.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("input").keypress(function(e){inputHandler(e)});
+	$("span").click(function(e){spanHandler(e)});
+});
+
+function inputHandler(e){
+	var chr = String.fromCharCode(e.charCode);
+	$("p").append(chr);
+}
+
+function spanHandler(e){
+  var chrCode = e.target.innerHTML.charCodeAt(0);
+  $("input").trigger({'type':'keypress', 'charCode':chrCode});        
+}
